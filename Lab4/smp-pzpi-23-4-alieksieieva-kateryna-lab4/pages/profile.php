a4a268bbde7e4da597a94aa25417bf42a5d2e3f1
NureAlieksieievaKateryna <kateryna.alieksieieva@nure.ua>
2025-05-30 17:05:09 +0300

    Delete Lab4/smp-pzpi-23-4-alieksieieva-kateryna-lab4/pages/profile.php

--- Lab4/smp-pzpi-23-4-alieksieieva-kateryna-lab4/pages/profile.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  'name' => 'Kateryna',
-  'surname' => 'Alieksieieva',
-  'birthdate' => '2005-08-11',
-  'bio' => 'Some description фівввввthat is long enough to pass validation...asdfwsdfsdfsdfsd',
-  'photo' => 'assets/profile_68376737683de.jpg',
-);
